23fe3f3e7d03bcd25b96029b0a03c309ecf551b8
Mark Anthony Vale <41247225+markavale@users.noreply.github.com>
2020-09-11 18:56:00 +0800

    fix errors on get request

--- weather/settings.py
+++ weather/settings.py
@@ -14,8 +14,8 @@ SECRET_KEY = 'mgj-^c*_kqq#qei=ws0m&x2(z-$*ajos4m69f$$rx1w*$ou^p)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['projectweatherapi.herokuapp.com', 'www.projectweatherapi.herokuapp.com']
-# ALLOWED_HOSTS = []#https://projectweatherapi.herokuapp.com/
+# ALLOWED_HOSTS = ['projectweatherapi.herokuapp.com', 'www.projectweatherapi.herokuapp.com']
+ALLOWED_HOSTS = []#https://projectweatherapi.herokuapp.com/
 
 # Application definition
 
