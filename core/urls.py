3482b07f28a7bf47739ea30b94aa9f1577314107
Mark Anthony Vale <41247225+markavale@users.noreply.github.com>
2020-09-12 15:45:11 +0800

    final version

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,7 @@ from .views import (city_create_view, index,
             list_create_city_view, add_coordinates)
 
 urlpatterns = [
-    path('', index, name="city-list"),
+    path('', index, name="index"),
     path('api/city/', city_create_view, name="city-post"),
     path('api/city/weather/', list_create_city_view, name="city-weather"),
     path('api/city/weather/coordinates/', add_coordinates, name="city-weather-coordinates"),
